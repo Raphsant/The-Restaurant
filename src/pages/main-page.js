a220af755d5e56fc15686f9fd1fbf103e8f25670
Rafael Santana <rsantana@colum.edu>
2022-04-29 15:28:09 -0500

    CSS changes

--- src/pages/main-page.js
+++ src/pages/main-page.js
@@ -2,12 +2,17 @@ import React from "react";
 import Navbar from "../components/navbar";
 import "../styles/main.css";
 import Menu from "./menu";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const MainPage = () => {
   return (
     <div id={"wrapper"}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" name={"Menu"} element={<Menu />} />
+        </Routes>
+      </BrowserRouter>
       <Navbar />
-      <Menu />
     </div>
   );
 };
